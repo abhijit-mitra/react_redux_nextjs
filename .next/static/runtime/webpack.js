249b5ed702c66b86dc06bd653119fac546fb1c44
Abhijit Mitra <abhijit@Apples-MacBook-Pro.local>
2020-02-08 19:21:37 +0530

    added server.js file

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5bb901113fd947fb9f11";
+/******/ 	var hotCurrentHash = "d2a6fc9395c936a09331";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
