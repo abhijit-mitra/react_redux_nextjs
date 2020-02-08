5525a17e619b75e778515d4a7eec978a933bf29f
Abhijit Mitra <abhijit@Apples-MacBook-Pro.local>
2020-02-08 17:56:03 +0530

    boilerplate setup

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "44d718d49770acf9ca26";
+/******/ 	var hotCurrentHash = "bb45f2b25e6182512b4c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
