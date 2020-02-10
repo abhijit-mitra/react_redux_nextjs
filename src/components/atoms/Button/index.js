62736c9616adf957e1c9dedb9661297a85acb2e6
Abhijit Mitra <abhijitmitra.in@gmail.com>
2020-02-10 13:45:06 +0530

    added precommit check

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,10 +1,11 @@
+import {memo} from 'React';
 import PropTypes from 'prop-types';
 
 import styles from './Button.module.css';
 
-const Button = ({children, ...rest}) => (
+const Button = memo(({children, ...rest}) => (
   <button {...rest} className={styles.primary}>{children}</button>
-);
+));
 
 Button.proTypes={
   children: PropTypes.element.isRequired,
